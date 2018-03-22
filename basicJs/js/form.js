253961f96c92292b6ffe78915129115645bd33fa
Witold Kupś <witold.kups@interlan.pl>
2018-03-22 21:36:32 +0100

    Global error info after register, edit info in table

--- basicJs/js/form.js
+++ basicJs/js/form.js
@@ -10,30 +10,61 @@ const FN_INPUT = "first-name";
 const LN_INPUT = "last-name";
 const validityFuncs = [validateFirstName, validateLastName, validateEmail, validatePassword, validateRepeatedPassword, validateDateOfBirth];
 const validityMap = {};
-function validateInput(inp, error) {
-    if (!error) {
+const errorsDiv = document.getElementById("errors");
+let showErrors;
+
+function updateErrors() {
+    if (showErrors) {
+        const errorStr = Object.keys(validityMap)
+            .map(k => {
+                const key = k.replace(/-/g, " ").replace(/(^| )(\w)/g, s => s.toUpperCase())
+                const value = validityMap[k];
+                if (value) {
+                    return `${key}: ${value}`
+                }
+            })
+            .filter(x => x)
+            .reduce((add, acc) => `${acc}\n${add}`);
+        if (errorStr.trim().length === 0) {
+            errorsDiv.style.display = "none";
+            showErrors = false;
+        } else {
+            errorsDiv.style.display = "block";
+            errorsDiv.innerText = errorStr;
+        }
+    } else {
+        errorsDiv.style.display = "none"
+    }
+}
+
+function validateInput(inp, error, skipErrorsListUpdate) {
+    if (error) {
+        inp.classList.remove(validClassName);
+        inp.classList.add(invalidClassName);
+    } else {
         inp.classList.remove(invalidClassName);
         inp.classList.add(validClassName);
-    } else {
-        inp.classList.add(invalidClassName);
-        inp.classList.remove(validClassName);
+    }
+    if (!skipErrorsListUpdate) {
+        updateErrors()
     }
     document.querySelector(`#${inp.id} ~ .invalid-feedback`).innerHTML = error;
+    updateErrors();
     validityMap[inp.id] = error;
 }
 
-function validatePassword() {
+function validatePassword(skipErrorsListUpdate) {
     const input = document.getElementById(PAS_INPUT);
     let error;
     if (!input.value)
         error = "You must repeat your password";
     else if (!pasReg.test(input.value))
         error = "Password must have at least one digit and 8 sings";
-    validateInput(input, error);
+    validateInput(input, error, skipErrorsListUpdate);
     return error;
 }
 
-function validateRepeatedPassword() {
+function validateRepeatedPassword(skipErrorsListUpdate) {
     const input = document.getElementById(PAS_REP_INPUT);
     let error;
     if (!input.value)
@@ -42,29 +73,29 @@ function validateRepeatedPassword() {
         error = "Password must have at least one digit and 8 sings";
     else if (input.value !== document.getElementById(PAS_INPUT).value)
         error = "Repeated and original password are not equal";
-    validateInput(input, error);
+    validateInput(input, error, skipErrorsListUpdate);
     return error;
 }
 
-function validateInputNotEmpty(id, inpName) {
+function validateInputNotEmpty(id, inpName, skipErrorsListUpdate) {
     const input = document.getElementById(id);
     let error;
     if (!input.value) {
         error = `${inpName} is required`
     }
-    validateInput(input, error);
+    validateInput(input, error, skipErrorsListUpdate);
     return error;
 }
 
-function validateLastName() {
-    return validateInputNotEmpty(LN_INPUT, "Last name");
+function validateLastName(skipErrorsListUpdate) {
+    return validateInputNotEmpty(LN_INPUT, "Last name", skipErrorsListUpdate);
 }
 
-function validateFirstName() {
-    return validateInputNotEmpty(FN_INPUT, "First name");
+function validateFirstName(skipErrorsListUpdate) {
+    return validateInputNotEmpty(FN_INPUT, "First name", skipErrorsListUpdate);
 }
 
-function validateEmail() {
+function validateEmail(skipErrorsListUpdate) {
     const emailInput = document.getElementById(EMAIL_INPUT);
     let error;
     if (!emailInput.value)
@@ -72,11 +103,11 @@ function validateEmail() {
     else if (!emailReg.test(emailInput.value))
         error = "Email is not valid";
 
-    validateInput(emailInput, error);
+    validateInput(emailInput, error, skipErrorsListUpdate);
     return error;
 }
 
-function validateDateOfBirth() {
+function validateDateOfBirth(skipErrorsListUpdate) {
     const dateOfBirth = document.getElementById(DATE_OF_BIRTH_INPUT);
     const valueAsDate = dateOfBirth.valueAsDate;
     let error;
@@ -84,13 +115,15 @@ function validateDateOfBirth() {
         error = "Date of birth is required";
     else if (new Date() - valueAsDate < 0)
         error = "Date of birth is invalid";
-    validateInput(dateOfBirth, error);
+    validateInput(dateOfBirth, error, skipErrorsListUpdate);
     return error;
 }
 
 function onSubmit(event) {
     let forms = document.querySelector('.needs-validation');
-    const errors = validityFuncs.map(f => f()).filter(r => r);
+    const errors = validityFuncs.map(f => f(true)).filter(r => r);
+    showErrors = errors.length > 0;
+    updateErrors();
     event.preventDefault();
     event.stopPropagation();
     forms.classList.add('was-validated');
--- basicJs/js/power-table.js
+++ basicJs/js/power-table.js
@@ -6,19 +6,24 @@ function getTable() {
 
 function updateRowActions(row, rowNum) {
     row.cells[0].outerHTML = `<th scope='row'>${rowNum}</th>`;
-    row.cells[3].innerHTML = `<span class="remove-power" onclick="removePower(${rowNum-1})">&#8211</span>`
+    row.cells[3].innerHTML = `
+    <span class="remove-power" onclick="removePower(${rowNum - 1})" class="close" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </span>`
+}
+
+function createEditableCell() {
+    return `<div contenteditable="true"></div>`
 }
 
 function addPower() {
     const table = getTable();
     const row = table.insertRow(-1);
     row.insertCell(0);
-    const cell1 = row.insertCell(1);
-    const cell2 = row.insertCell(2);
+    row.insertCell(1).innerHTML = createEditableCell();
+    row.insertCell(2).innerHTML = createEditableCell();
     row.insertCell(3);
     const rowNum = table.rows.length;
-    cell1.innerHTML = " ";
-    cell2.innerHTML = "  ";
     updateRowActions(row, rowNum)
 }
 
@@ -26,6 +31,6 @@ function removePower(index) {
     const table = getTable();
     table.deleteRow(index);
     for (let i = 0; i < table.rows.length; i++) {
-        updateRowActions(table.rows[i], i+1);
+        updateRowActions(table.rows[i], i + 1);
     }
 }
