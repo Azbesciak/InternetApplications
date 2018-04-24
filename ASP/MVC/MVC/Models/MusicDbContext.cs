96e5f2d5cfc6c17db30c095f48179858708ba501
Witold Kupś <witold.kups@interlan.pl>
2018-04-24 12:07:02 +0200

    add ASP.MVC files

---
+++ ASP/MVC/MVC/Models/MusicDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class MusicDbContext: DbContext
+    {
+//        public MusicDbContext() : base("DefaultConnection")
+//        {
+//        }
+
+        public DbSet<Song> Songs { get; set; }
+
+        public System.Data.Entity.DbSet<MVC.Models.Genre> Genres { get; set; }
+    }
+}
